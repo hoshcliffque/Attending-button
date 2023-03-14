8e3d617e9f33f7bd71b2f54433d7466c491820e1
Enrique Khoo <enrique1698@yahoo.com>
2023-03-14 20:22:54 +0000

    first commit

---
+++ event_bulb/event_bulb_start/events/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from datetime import datetime
+from .models import Event
+from accounts.models import UserProfile
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def add_attending(request, id):
+    event = get_object_or_404(Event, id=id)
+    if request.method == "POST":
+        request.user.profile.attending.add(event)
+    return redirect("events_list")  
+
+@login_required
+def remove_attending(request, id):
+    event = get_object_or_404(Event, id=id)
+    if request.method == "POST":
+        request.user.profile.attending.remove(event)
+    return redirect("events_list")  
+
+    
+def details(request, id):
+    event = get_object_or_404(Event, id=id)
+    return render(request, 'events/details.html', {'event': event})
+
+
+def list(request):
+    today = datetime.today()
+
+    filter_map = {
+        'title': 'title__icontains',
+        'is_free': 'cost__exact'
+    }
+
+    filters = {}
+    for key, value in request.GET.items():
+        filter_key = filter_map[key]
+        filters[filter_key] = value
+
+    events = Event.objects.filter(
+        datetime__gte=today).filter(**filters).order_by('datetime')
+    return render(request, 'events/list.html', {'events': events})
+
+
